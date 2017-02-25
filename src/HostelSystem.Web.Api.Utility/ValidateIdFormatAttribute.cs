741981b3ce7112ca7bcc1e1e9f3894dec5182d38
Michał Głowaczewski <michalglowaczewski@gmail.com>
2017-02-25 21:23:54 +0100

    Dodano kontroler obsługujący rezerwacje

---
+++ src/HostelSystem.Web.Api.Utility/ValidateIdFormatAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace HostelSystem.Web.Api.Utility
+{
+    public class ValidateIdFormatAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.ActionArguments.ContainsKey("id"))
+            {
+
+                try
+                {
+                    var id = (Guid)actionContext.ActionArguments["id"];
+                }
+                catch (NullReferenceException e)
+                {
+                    actionContext.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                }
+            }
+        }
+    }
+}
---
+++ src/HostelSystem.Web.Api/Controllers/RezerwacjaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HostelSystem.Web.Api.Utility;
+
+namespace HostelSystem.Web.Api.Controllers
+{
+    public class RezerwacjaController : ApiController
+    {
+        private readonly IRezerwacjaService _service;
+
+        public RezerwacjaController(IRezerwacjaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok(await _service.Get());
+        }
+
+        [HttpGet]
+        [ValidateIdFormat]
+        public async Task<IHttpActionResult> Get(Guid id)
+        {
+            return Ok(await _service.Get(id));
+        }
+    }
+}
